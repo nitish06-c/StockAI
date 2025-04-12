7eb63e8b9016237645528474b83b1ee0779af4f9
Nitish Madabusi <nit.madabusi@gmail.com>
2025-04-12 19:12:05 -0400

    [Feat]: Added interactive elelements such as being able to pick stock symbol, ask gpt your own questions, goback to different stock symbol, and exit from anywhere. Also added a introduction area

--- StockAI/main.cpp
+++ StockAI/main.cpp
@@ -62,7 +62,7 @@ std::string gptCall(const std::string &analysis, const std::string& openaiKey) {
     std::string text = "Explain the trends: " + analysis;
     
     json payload;
-        payload["model"] = "gpt-3.5-turbo";
+        payload["model"] = "gpt-4o";
         payload["messages"] = {
             { {"role", "user"}, {"content", text} }
         };
@@ -103,13 +103,7 @@ std::string gptCall(const std::string &analysis, const std::string& openaiKey) {
 }
 
 
-int main(int argc, const char * argv[]) {
-    if (argc != 2) {
-        std::cout << "Usage: ./StockAI <TICKER>\n";
-        return 1;
-    }
-    
-    std::string ticker = argv[1];
+int main() {
     std::string stockApiKey = getEnvChar("STOCK_API_KEY");
     std::string openApiKey = getEnvChar("OPENAI_API_KEY");
     
@@ -118,13 +112,41 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
     
-    std::string stockdata = getData(ticker, stockApiKey);
-    std::string data = formatDataFromJson(stockdata, ticker);
-    std::string gptres = gptCall(data, openApiKey);
+    std::cout << "Welcome to StockAI! Get information on stocks here\n";
+    std::cout << "Type 'exit' anytime to quit or 'back' to choose a different stock\n";
 
-    std::cout << "\nStock Summary: " << data << "\n\n";
-    std::cout << "ChatGPT analysis:\n" << gptres << "\n";
+    while (true) {
+        std::string ticker;
+        std::cout << "Input stock symbol: ";
+        std::getline(std::cin, ticker);
+        
+        if (ticker == "exit") {
+            std::cout << "Exiting\n";
+            break;
+        }
+        std::string stockdata = getData(ticker, stockApiKey);
+        std::string data = formatDataFromJson(stockdata, ticker);
+        
+        while (true) {
+            std::string q;
+            std::cout << "Ask GPT about the stock " << ticker << " or type 'back' to pick a different stock: ";
+            std::getline(std::cin, q);
+            
+            if (q == "back") {
+                break;
+            }
+            if (q == "exit") {
+                std::cout << "Exiting\n";
+                exit(0);
+            }
+            std::string gptprompt = data + "\n" + q;
+            std::string gptres = gptCall(gptprompt, openApiKey);
 
+            std::cout << "\nStock Summary: " << data << "\n\n";
+            std::cout << "ChatGPT analysis: " << gptres << "\n";
+            
+        }
+        
+    }
     return 0;
-    
 }
