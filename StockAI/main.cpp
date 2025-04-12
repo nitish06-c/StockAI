8112ed03cbcae4e01ca30717b627a09b42388fc7
Nitish Madabusi <nit.madabusi@gmail.com>
2025-04-11 21:06:52 -0400

    Initial Commit

---
+++ StockAI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StockAI
+//
+//  Created by Nitish Madabusi on 2025-04-11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
